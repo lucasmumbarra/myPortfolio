b2af032be73e73d76d572b2d519376608d9519d2
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-03-02 12:55:33 -0300

    #commit020322
    Foi adicionado os Projetos, junto com os cards, direcionando para o diretório.

--- js/api.js
+++ js/api.js
@@ -4,13 +4,19 @@
   const user = "lucasmumbarra"
   const client_id = "aacc39334836ed5fa8bb"
   const client_secret = "30385253f2418d5284a86f46cfd08a11320f020b"
+  const count = 6;
+  const sort = "stargazers"
+  const direction = "asc"
 
   async function getUser(user){
     const profileResponse = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`)
 
-    const profile = profileResponse.json()
+    const reposResponse = await fetch(`${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`)
 
-    return profile
+    const profile = await profileResponse.json()
+    const repos = await reposResponse.json()
+
+    return {profile, repos}
   }
 
   function showProfile(user) {
@@ -23,5 +29,47 @@
    </div>`
   }
 
-  getUser(user).then(res => showProfile(res))
+  function showRepos(repos) {
+    let output = '';
+
+    repos.forEach(repo => {
+      output += `<div class="repo">
+        <a href=${repo.html_url} target="_blanck">
+          <div class="repoLineOne">
+            <img src="./assets/folder.svg" alt="Folder Github repository">
+            <h2>${repo.name}</h2>
+          </div>
+
+          <div class="repoLineTwo">
+            <p>${repo.description}</p>
+          </div>
+
+          <div class="repoLineTree">
+            <div class="repoGit">
+              <div class="gitStar">
+                <img src="./assets/star.svg" alt="Star Github repository">
+                <p>${repo.stargazers_count}</p>
+              </div>
+              <div class="gitBranch">
+                <img src="./assets/git-branch.svg" alt="Branch Github repository">
+                <p>${repo.forks_count}</p>
+              </div>
+            </div>
+            <div class="repoLanguage">
+              <div class="circleLanguage"></div>
+              <p>${repo.language}</p>
+            </div>
+          </div>
+        </a>
+       </div>
+      `
+    })
+
+    document.getElementById("repositories").innerHTML = output
+  }
+
+  getUser(user).then(res => {
+    showProfile(res.profile)
+    showRepos(res.repos)
+  })
 })()
